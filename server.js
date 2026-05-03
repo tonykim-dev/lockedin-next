a6c5dd373895f762963b7fbfa23d349feb250534
Tony Kim <tony@tonykim.dev>
2026-05-03 09:55:48 -0700

    feat: add GET /habits return empty array and 404 fallback

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from 'node:http';
 import fs from 'node:fs/promises';
 
 const server = http.createServer(async (req, res) => {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({
-        data: 'Hello World!',
+    if (req.method === 'GET'  && req.url === '/habits'){
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify({
+        data:"[ ] ",}))}
+    else {
+        res.writeHead(404,{'Content-Type':'application/json'});
+        res.end(JSON.stringify({
+        data: '404 endpoint does not exist.',
     }));
+    }
 });
 server.listen(8000);
