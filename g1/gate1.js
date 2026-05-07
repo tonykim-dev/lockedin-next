280c029be5e3dc41899ebdc4bbcb1c3ad4b04c34
Tony Kim <tony@tonykim.dev>
2026-05-07 14:34:12 -0700

    feat: add GET /habit endpoint

--- g1/gate1.js
+++ g1/gate1.js
@@ -1,1 +1,13 @@
-//
+import http from 'node:http';
+import fs from 'node:fs/promises';
+
+const server = http.createServer(async (req,res) => {
+    if (req.method === "GET" && req.url === "/habits"){
+        const habits = fs.readFile('habits.json','utf-8');
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(habits));
+        return;
+    }
+    res.end();
+});
+server.listen(3000);
